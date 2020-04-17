cf6df02cedf1d5a725b441a39671b2a831c6035f
Christopher <reinkendigital@gmail.com>
2020-04-17 14:13:14 -0500

    removed next and previous commands

--- lib/auction_crawler/auction.rb
+++ lib/auction_crawler/auction.rb
@@ -11,10 +11,9 @@ class Auction
         self.init_dis_items
         @runner = InfoRunner.new
         @auction_list = runner.get_auction_list
-        self.find_or_create_items_from_auction_list
     end
 
-    def find_or_create_items_from_auction_list(idx1=0,idx2=9)
+    def find_or_create_items_from_auction_list(idx1,idx2)
         # create items from @auction_list 10 at a time to minimize initial loading
         self.auctions[idx1..idx2].each do |list_item|
             AuctionItem.find_or_create_by_id(list_item['id'],@runner.get_item_by_id(list_item['item']['id']))
@@ -29,7 +28,7 @@ class Auction
 
     def display_list
         # get a 10 item chunk by index from AuctionItem.all
-        self.find_or_create_items_from_auction_list[@current_displayed_items[0]..@current_displayed_items[1]].each_with_index do |item, inx|
+        self.find_or_create_items_from_auction_list(@current_displayed_items[0],@current_displayed_items[1]).each_with_index do |item, inx|
             puts "#{inx+1}) #{item.name} #{item.item_subclass} #{item.level}"
         end
     end
@@ -54,10 +53,10 @@ class Auction
                 case input
                 when "list"
                     self.display_commands
-                when "next"
-                    self.next_10_items
-                when "prev"
-                    self.previous_10_items
+                # when "next"
+                #     self.next_10_items
+                # when "prev"
+                #     self.previous_10_items
                 when "refresh"
                     self.display_list
                 end
@@ -77,43 +76,47 @@ class Auction
         # displays all the currently viable commands
         puts "1-10 - picks an item to view more details"
         puts "list - display this list of commands"
-        puts "next - display the next 10 auction items"
-        puts "prev - display the previous 10 auction items"
+        # puts "next - display the next 10 auction items"
+        # puts "prev - display the previous 10 auction items"
         puts "refresh - shows the current list of 10 auction items"
         puts "exit - exits the program"
     end
 
-    def next_10_items
-        # adjust stored indexes of the currently displayed items to be the next
-        # 10 items
-        @current_displayed_items[0] += 10
-        @current_displayed_items[1] += 10
-
-        # check to see if there are any more items in the AuctionItem.all list.
-        # if there aren't set the stored index array back to what it was.
-        if self.auctions.length < (@current_displayed_items[0] + 1)
-            puts "no more items to display"
-            @current_displayed_items[0] -= 10
-            @current_displayed_items[1] -= 10
-        elsif self.auctions.length <= @current_displayed_items[1]
-            @current_displayed_items[1] = self.auctions.length-1
-            self.display_list
-        end
-    end
-
-    def previous_10_items
-        # adjust stored indexes of the currently displayed items to be the previous
-        # 10 items
-        @current_displayed_items[0] -= 10
-        @current_displayed_items[1] -= 10
-
-        # check to see if the stored index number is less than 0 and therefore
-        # out of bounds. if so reinitialize stored index array.
-        if @current_displayed_items[0] < 0
-            self.init_dis_items
-        end
-        self.display_list
-    end
+    # def next_10_items
+    #     # adjust stored indexes of the currently displayed items to be the next
+    #     # 10 items
+    #     @current_displayed_items[0] += 10
+    #     @current_displayed_items[1] += 10
+    #
+    #     # check to see if there are any more items in the AuctionItem.all list.
+    #     # if there aren't set the stored index array back to what it was.
+    #     if self.auctions.length < (@current_displayed_items[0] + 1)
+    #         puts "no more items to display"
+    #         @current_displayed_items[0] -= 10
+    #         @current_displayed_items[1] -= 10
+    #     elsif self.auctions.length <= @current_displayed_items[1]
+    #         @current_displayed_items[1] = self.auctions.length-1
+    #         self.find_or_create_items_from_auction_list(@current_displayed_items[0],@current_displayed_items[1])
+    #         self.display_list
+    #     else
+    #         self.find_or_create_items_from_auction_list(@current_displayed_items[0],@current_displayed_items[1])
+    #         self.display_list
+    #     end
+    # end
+    #
+    # def previous_10_items
+    #     # adjust stored indexes of the currently displayed items to be the previous
+    #     # 10 items
+    #     @current_displayed_items[0] -= 10
+    #     @current_displayed_items[1] -= 10
+    #
+    #     # check to see if the stored index number is less than 0 and therefore
+    #     # out of bounds. if so reinitialize stored index array.
+    #     if @current_displayed_items[0] < 0
+    #         self.init_dis_items
+    #     end
+    #     self.display_list
+    # end
 
     def init_dis_items
         #creates stored index array used for displaying auction items
